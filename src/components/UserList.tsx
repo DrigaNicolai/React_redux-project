38d6ea611b15fe85af80fb8f2fa84d6ce0ffe6f2
DrigaNicolai <just.whyme7@gmail.com>
2023-03-11 11:20:27 +0200

    Action creator + types

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../store/action-creators/user";
 
 const UserList: React.FC = () => {
-  const {users, error, loading}  = useTypedSelector(state => state.user)
+  const {users, error, loading}  = useTypedSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // @ts-ignore
+    dispatch(fetchUsers())
+  }, []);
+
+  if (loading) {
+    return (
+      <h2>Loading...</h2>
+    );
+  }
+
+  if (error) {
+    return (
+      <h2>{error}</h2>
+    );
+  }
 
   return (
     <div>
-      
+      {users.map(user =>
+          <div key={user.id}>{user.name}</div>
+        )
+      }
     </div>
   );
 };
