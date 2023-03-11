6512b40c5d12a4c7318cf02cf18c8a0e9ee714aa
DrigaNicolai <just.whyme7@gmail.com>
2023-03-11 12:03:06 +0200

    TodoList component. Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import UserList from "./components/UserList";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <h1>Redux project</h1>
       <UserList />
+      <hr />
+      <TodoList />
     </div>
   );
 }
